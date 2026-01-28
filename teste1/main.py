2cfa02d64c19f16a09f3b5cc5a5df51ccd6a1145
Emilly Cavalcante <emilly.menezescs@gmail.com>
2026-01-28 20:56:22 -0300

    feat(teste1): add extract_zip function to process files

--- teste1/main.py
+++ teste1/main.py
@@ -1,4 +1,7 @@
 import requests
+import zipfile
+import pandas as pd
+from io import BytesIO
 
 def download_file(url: str):
     """Baixa arquivo ZIP da URL e retorna o conteúdo em bytes"""
@@ -18,3 +21,22 @@ def download_file(url: str):
     except requests.exceptions.RequestException as e:
         print(f"Erro de conexão ao baixar arquivo: {e}")
         return None
+    
+def extract_zip(zip_content: bytes):
+    """Extrai o conteúdo do arquivo ZIP que foi baixado e retorna um DataFrame"""
+
+    zip_file = BytesIO(zip_content)
+
+    with zipfile.ZipFile(zip_file, mode='r') as zip:
+        files = zip.namelist()
+
+        for file in files:
+            if file.endswith('.csv'):
+                csv_filename = file
+
+        csv_content = zip.read(csv_filename)
+        
+        csv_buffer = BytesIO(csv_content)
+        df = pd.read_csv(csv_buffer, delimiter=';', decimal=',')
+
+        return df
